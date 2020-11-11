1cf2b5404bae243704cc09dd069e2a2afeeb2a22
unknown <g00346194@gmit.ie>
2020-11-11 11:37:21 +0000

    Lab Upload

--- src/components/create.js
+++ src/components/create.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import axios from 'axios';
 
 export class Create extends React.Component {
 
@@ -40,6 +40,19 @@ export class Create extends React.Component {
     onSubmit(e) {
         e.preventDefault();
         alert("Movie: " + this.state.Title + " " + this.state.Year +" "+ this.state.Poster);
+
+        const newMovie = {
+            title: this.state.Title,
+            year: this.state.Year,
+            poster: this.state.Poster
+        }
+        axios.post('http://localhost:4000/api/movies',newMovie)
+        .then((res)=>{
+            console.log(res)
+        })
+        .catch((err) => {
+            console.log(err)
+        })
     }
 
 
