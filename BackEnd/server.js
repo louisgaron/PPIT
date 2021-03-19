d64a11cc19c4f470f93bc3b3bc09d4d13feb2087
louisgaron <g00346194@gmit.ie>
2021-03-19 09:52:31 +0000

    Update server.js

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -45,37 +45,6 @@ app.use(function (req, res, next) {
 
 app.get('/api/movies', (req, res) => {
 
-    // const myovies = [
-    //     {
-    //         "Title": "Avengers: Infinity War",
-    //         "Year": "2018",
-    //         "imdbID": "tt4154756",
-    //         "Type": "movie",
-    //         "Poster": "https://m.media-amazon.com/images/M/MV5BMjMxNjY2MDU1OV5BMl5BanBnXkFtZTgwNzY1MTUwNTM@._V1_SX300.jpg"
-    //     },
-    //     {
-    //         "Title": "Captain America: Civil War",
-    //         "Year": "2016",
-    //         "imdbID": "tt3498820",
-    //         "Type": "movie",
-    //         "Poster": "https://m.media-amazon.com/images/M/MV5BMjQ0MTgyNjAxMV5BMl5BanBnXkFtZTgwNjUzMDkyODE@._V1_SX300.jpg"
-    //     },
-    //     {
-    //         "Title": "World War Z",
-    //         "Year": "2013",
-    //         "imdbID": "tt0816711",
-    //         "Type": "movie",
-    //         "Poster": "https://m.media-amazon.com/images/M/MV5BNDQ4YzFmNzktMmM5ZC00MDZjLTk1OTktNDE2ODE4YjM2MjJjXkEyXkFqcGdeQXVyNTA4NzY1MzY@._V1_SX300.jpg"
-    //     },
-    //     {
-    //         "Title": "War of the Worlds",
-    //         "Year": "2005",
-    //         "imdbID": "tt0407304",
-    //         "Type": "movie",
-    //         "Poster": "https://m.media-amazon.com/images/M/MV5BNDUyODAzNDI1Nl5BMl5BanBnXkFtZTcwMDA2NDAzMw@@._V1_SX300.jpg"
-    //     }
-    // ];
-
     movieModel.find((err, data) => {
         res.json(data);
     })
